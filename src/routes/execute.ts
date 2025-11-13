c311ac016bd0dbfee504fed35fb340526799963f
Coconhat <nhat_vu_le@dlsl.edu.ph>
2025-11-14 00:00:50 +0800

    feat: add Gemini AI to get user's message and turn it into json format for the Foursquare Places API

--- src/routes/execute.ts
+++ src/routes/execute.ts
@@ -1,20 +1,47 @@
+import { GoogleGenAI } from "@google/genai";
 import Router from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   console.log("query:", req.query);
 
   const { message, code } = req.query;
 
+  // Auth check
   if (code !== process.env.AUTH_CODE) {
     return res.status(401).json({ error: "Unauthorized" });
   }
 
-  res.json({
-    success: true,
-    message: `Received: ${message}`,
-  });
+  // Initialize Google GenAI client
+  const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY! });
+
+  // Generate JSON content based on user message
+  try {
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: `Your job is to convert the user's message into a json format for the Foursquare Places API,
+
+      it should be like this: 
+      {
+            "action": "restaurant_search",
+            "parameters": {
+            "query": "sushi",
+            "near": "downtown Los Angeles",
+            "price": "1",
+            "open_now": true
+  }
+      
+      user message: ${message}`,
+    });
+    res.json({
+      success: true,
+      aiResponse: response.text,
+    });
+  } catch (error) {
+    console.error("Error generating content:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 export default router;
